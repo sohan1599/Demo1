1d16227916cc00abd0ecdbb8b479fe9c60b5a8fa
sohan <sohanmaindarkar57@gmail.com>
2022-05-05 22:02:35 +0530

    My first GIT project

---
+++ src/ABC/Demo.java
@@ -0,0 +1,13 @@
+package ABC;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		System.out.println(a);
+
+	}
+
+}
---
+++ src/Github/Demo1.java
@@ -0,0 +1,14 @@
+package Github;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		System.out.println(a);
+
+	}
+
+}
