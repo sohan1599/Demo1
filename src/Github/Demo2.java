5469446c5dfa3fb820e494dc2a2d9dc79ead4796
sohan <sohanmaindarkar57@gmail.com>
2022-05-05 22:24:54 +0530

    My 2nd github project

---
+++ src/Github/Demo2.java
@@ -0,0 +1,15 @@
+package Github;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		
+		int b=25;
+		
+		System.out.println(a+b);
+				
+	}
+
+}
